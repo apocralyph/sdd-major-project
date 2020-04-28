4762a1c29a3a909109b5f4f0d4bbcabc5770d36e
apocralyph <60494823+apocralyph@users.noreply.github.com>
2020-04-28 11:40:43 +1000

    Fixed GUI

--- SDD_Major/mainWindow.py
+++ SDD_Major/mainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file 'mainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
@@ -14,7 +14,7 @@ from PyQt5.QtCore import QObject, pyqtSlot
 class Ui_MainWindow(QObject):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(814, 649)
+        MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
@@ -22,22 +22,7 @@ class Ui_MainWindow(QObject):
         self.splitter = QtWidgets.QSplitter(self.centralwidget)
         self.splitter.setOrientation(QtCore.Qt.Vertical)
         self.splitter.setObjectName("splitter")
-        self.widget = QtWidgets.QWidget(self.splitter)
-        self.widget.setObjectName("widget")
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.widget)
-        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.label = QtWidgets.QLabel(self.widget)
-        self.label.setMaximumSize(QtCore.QSize(200, 16777215))
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setWordWrap(True)
-        self.label.setObjectName("label")
-        self.horizontalLayout_3.addWidget(self.label)
-        self.frame = QtWidgets.QFrame(self.widget)
+        self.frame = QtWidgets.QFrame(self.splitter)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
@@ -45,16 +30,16 @@ class Ui_MainWindow(QObject):
         self.gridLayout.setObjectName("gridLayout")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label_2 = QtWidgets.QLabel(self.frame)
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout.addWidget(self.label_2)
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
         self.lineEdit = QtWidgets.QLineEdit(self.frame)
         self.lineEdit.setObjectName("lineEdit")
         self.horizontalLayout.addWidget(self.lineEdit)
         self.pushButton = QtWidgets.QPushButton(self.frame)
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout.addWidget(self.pushButton)
-        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 3)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.originalTextBrowser = QtWidgets.QTextBrowser(self.frame)
@@ -63,40 +48,50 @@ class Ui_MainWindow(QObject):
         self.translatedTextBrowser = QtWidgets.QTextBrowser(self.frame)
         self.translatedTextBrowser.setObjectName("translatedTextBrowser")
         self.horizontalLayout_2.addWidget(self.translatedTextBrowser)
-        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 1)
-        self.horizontalLayout_3.addWidget(self.frame)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 3)
+        spacerItem = QtWidgets.QSpacerItem(335, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 2, 0, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 2, 1, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(334, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 2, 2, 1, 1)
         self.debugTextBrowser = QtWidgets.QTextBrowser(self.splitter)
         self.debugTextBrowser.setObjectName("debugTextBrowser")
         self.gridLayout_2.addWidget(self.splitter, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 814, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.actionAbout = QtWidgets.QAction(MainWindow)
-        self.actionAbout.setObjectName("actionAbout")
 
         self.retranslateUi(MainWindow)
         self.pushButton.clicked.connect(self.browseSlot)
         self.lineEdit.returnPressed.connect(self.returnPressedSlot)
+        self.pushButton_2.clicked.connect(self.translateSlot)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Japanese to English Dictionary"))
-        self.label_2.setText(_translate("MainWindow", "File Name"))
+        self.label.setText(_translate("MainWindow", "File Name"))
         self.pushButton.setText(_translate("MainWindow", "Browse"))
-        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.pushButton_2.setText(_translate("MainWindow", "Translate"))
 
     def browseSlot(self):
         pass
 
     def returnPressedSlot(self):
         pass
+
+    def translateSlot(self):
+        pass
+
+    def 
+
 
 if __name__ == "__main__":
     import sys
